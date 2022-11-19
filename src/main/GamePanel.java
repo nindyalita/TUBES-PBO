da5c96483363243b25aaab56c869a6a5f732ac0b
nindyalita <nindyalita@gmail.com>
2022-11-19 12:13:14 +0800

    progress tubes pbo

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,26 +1,68 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // screen settings
-    final int orginalTileSize = 16;
-    final int scale = 3;
+    final int orginalTileSize = 16; // original size of character or object
+    final int scale = 3; // we make object with 16x16 pixel but lok in the screen in 48x48
 
-    final int tileSize = orginalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;
-    final int screenHeight = tileSize * maxScreenRow;
+    final int tileSize = orginalTileSize * scale; // 48x48
+    final int maxScreenCol = 16;// max col to see in the screen
+    final int maxScreenRow = 12;// max row to see in the screen
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixel
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixel
+
+    KeyHandler keyH = new KeyHandler();
+
+    // actually object in game is static but it like move because in 60 s the object
+    // is actually move so it look like real, to make this we make clock on the game
+    // with thread
+    Thread gameThread;
 
     public GamePanel() {
+        // size of jpanel or content
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH); // add keylistener to gamepanel
+        this.setFocusable(true);
     }
 
+    public void startGameThread() {
+        gameThread = new Thread(this);// add jpanel to thread
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            // System.out.println("running loop game");
+
+            // 1, UPDATE : update ifnoramtion such as character position
+            update();
+            // 2. DRAW : draw the screen tih the updated information
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) { // standart method to draw object in java
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,13 +6,17 @@ public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setTitle("Game");
+        window.setResizable(false); // cannot resized window
+        window.setTitle("Treasure Hunting Game");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel); // add gamepanel to frame
 
-        window.setLocationRelativeTo(null);
+        window.pack(); // frame menyesuaikan dengan gamepanel/konten
+
+        window.setLocationRelativeTo(null); // setwindow in center
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
