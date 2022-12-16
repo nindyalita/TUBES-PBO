bf50fe465a2bcc640b8d2e2a9a9cb3cbb63714ef
nindyalita <nindyalita@gmail.com>
2022-12-16 18:28:06 +0800

    fixing current inventory items

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -90,9 +90,6 @@ public class Player extends Entity {
         inventory.clear();
         inventory.add(currentWeapon);
         inventory.add(currentShield);
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
 
     }
 
